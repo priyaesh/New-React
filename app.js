bc11c6ac00010b40fc5f1850444e7252e74b1cb8
priyaEsh <hemapriyasampath@gmail.com>
2025-02-13 12:11:46 -0500

    app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+//const heading =React.createElement("h1",{})
+
+const heading=React.createElement("h1",{}, "This is from REACT");
+
+ const root=ReactDOM.createRoot(document.getElementById("root"));
+ 
+root.render(heading);

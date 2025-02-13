6a16f9e26c801f36d4b5325068e928f6294e15a0
priyaEsh <hemapriyasampath@gmail.com>
2025-02-13 12:17:30 -0500

    create element with -{element, attribute, child}

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //const heading =React.createElement("h1",{})
 
-const heading=React.createElement("h1",{}, "This is from REACT");
+const heading=React.createElement("h1",{id:"heading",xya:"skdl"}, "This is from REACT");
+
+console.log(heading);
 
  const root=ReactDOM.createRoot(document.getElementById("root"));
  
